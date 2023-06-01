b47bac2d71f4c2d61dee75d006cb513ab232913d
haraldpaaske <haraldpaaske@gmail.com>
2023-06-01 17:10:04 +0200

    image and greet

---
+++ harald/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+
+function App() {
+  return (
+    <div className="App">
+      <div className='image_of_me'>
+        <img src="https://media.licdn.com/dms/image/D4D03AQEOKJDE_GShcA/profile-displayphoto-shrink_800_800/0/1681821615348?e=1691020800&v=beta&t=uyUvUfuaLi7ksyobqD9KuVsINGYft-G0gVC98iVG5YE"></img>   
+      </div>
+      <p>Hello</p>
+    </div>
+  );
+}
+
+export default App;
